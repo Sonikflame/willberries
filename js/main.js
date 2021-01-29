04baf300fdd21e76bde66c37962c5351a7a7bb7a
Sonikflame <47040623+Sonikflame@users.noreply.github.com>
2021-01-29 14:24:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  
+   
+  });
+  
